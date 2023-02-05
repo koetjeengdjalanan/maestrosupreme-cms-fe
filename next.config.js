7875fb7baf4ac4b2fdc84e428f65f25732368bd3
edwardedo603 <edwardedo603@gmail.com>
2023-02-05 13:07:54 +0700

    refactor: 💡 next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    trailingSlash: true,
-    basePath: process.env.NODE_ENV === 'production' ? '/sakai-react' : '',
-    publicRuntimeConfig: {
-        contextPath: process.env.NODE_ENV === 'production' ? '/sakai-react' : '',
-        uploadPath:
-            process.env.NODE_ENV === 'production' ? '/sakai-react/upload.php' : '/api/upload',
-    },
+  reactStrictMode: false,
+  trailingSlash: true,
+  // basePath: process.env.NODE_ENV === 'production' ? '/sakai-react' : '',
+  publicRuntimeConfig: {
+    //   contextPath: process.env.NODE_ENV === 'production' ? '/sakai-react' : '',
+    contextPath: '',
+    //   uploadPath:
+    //       process.env.NODE_ENV === 'production' ? '/sakai-react/upload.php' : '/api/upload',
+    uploadPath: '/api/upload',
+  },
 };
 
 module.exports = nextConfig;
