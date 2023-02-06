9a51872ce1ac3d87d7f14ed9c5006974ffc3a90c
KoetjeengDjalanan <maritnsambulare@gmail.com>
2023-02-07 03:34:56 +0700

    HalfDone

--- src/layout/AppTopbar.js
+++ src/layout/AppTopbar.js
@@ -11,6 +11,7 @@ import { LayoutContext } from './context/layoutcontext';
 import { TieredMenu } from 'primereact/tieredmenu';
 import Image from 'next/image';
 import { signOut } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 const AppTopbar = forwardRef((props, ref) => {
   const { layoutState, onMenuToggle, showProfileSidebar } =
@@ -20,6 +21,7 @@ const AppTopbar = forwardRef((props, ref) => {
   const topbarmenubuttonRef = useRef(null);
   const contextPath = getConfig().publicRuntimeConfig.contextPath;
   const profileMenuButtonRef = useRef(null);
+  const router = useRouter();
 
   useImperativeHandle(ref, () => ({
     menubutton: menubuttonRef.current,
@@ -28,121 +30,11 @@ const AppTopbar = forwardRef((props, ref) => {
   }));
   const items = [
     {
-      label: 'File',
-      icon: 'pi pi-fw pi-file',
-      items: [
-        {
-          label: 'New',
-          icon: 'pi pi-fw pi-plus',
-          items: [
-            {
-              label: 'Bookmark',
-              icon: 'pi pi-fw pi-bookmark',
-            },
-            {
-              label: 'Video',
-              icon: 'pi pi-fw pi-video',
-            },
-          ],
-        },
-        {
-          label: 'Delete',
-          icon: 'pi pi-fw pi-trash',
-        },
-        {
-          separator: true,
-        },
-        {
-          label: 'Export',
-          icon: 'pi pi-fw pi-external-link',
-        },
-      ],
-    },
-    {
-      label: 'Edit',
-      icon: 'pi pi-fw pi-pencil',
-      items: [
-        {
-          label: 'Left',
-          icon: 'pi pi-fw pi-align-left',
-        },
-        {
-          label: 'Right',
-          icon: 'pi pi-fw pi-align-right',
-        },
-        {
-          label: 'Center',
-          icon: 'pi pi-fw pi-align-center',
-        },
-        {
-          label: 'Justify',
-          icon: 'pi pi-fw pi-align-justify',
-        },
-      ],
-    },
-    {
-      label: 'Users',
-      icon: 'pi pi-fw pi-user',
-      items: [
-        {
-          label: 'New',
-          icon: 'pi pi-fw pi-user-plus',
-        },
-        {
-          label: 'Delete',
-          icon: 'pi pi-fw pi-user-minus',
-        },
-        {
-          label: 'Search',
-          icon: 'pi pi-fw pi-users',
-          items: [
-            {
-              label: 'Filter',
-              icon: 'pi pi-fw pi-filter',
-              items: [
-                {
-                  label: 'Print',
-                  icon: 'pi pi-fw pi-print',
-                },
-              ],
-            },
-            {
-              icon: 'pi pi-fw pi-bars',
-              label: 'List',
-            },
-          ],
-        },
-      ],
-    },
-    {
-      label: 'Events',
-      icon: 'pi pi-fw pi-calendar',
-      items: [
-        {
-          label: 'Edit',
-          icon: 'pi pi-fw pi-pencil',
-          items: [
-            {
-              label: 'Save',
-              icon: 'pi pi-fw pi-calendar-plus',
-            },
-            {
-              label: 'Delete',
-              icon: 'pi pi-fw pi-calendar-minus',
-            },
-          ],
-        },
-        {
-          label: 'Archieve',
-          icon: 'pi pi-fw pi-calendar-times',
-          items: [
-            {
-              label: 'Remove',
-              icon: 'pi pi-fw pi-calendar-minus',
-            },
-          ],
-        },
-      ],
+      label: 'Profile',
+      icon: 'pi pi-fw pi-user-edit',
+      command: () => {
+        router.push('/user');
+      },
     },
     {
       separator: true,
@@ -195,9 +87,15 @@ const AppTopbar = forwardRef((props, ref) => {
           'layout-topbar-menu-mobile-active': layoutState.profileSidebarVisible,
         })}
       >
-        <button type="button" className="p-link layout-topbar-button">
-          <i className="pi pi-calendar"></i>
-          <span>Calendar</span>
+        <button
+          type="button"
+          className="p-link layout-topbar-button"
+          onClick={() => {
+            router.push('/user/management');
+          }}
+        >
+          <i className="pi pi-sitemap"></i>
+          <span>CMS Management</span>
         </button>
         <button
           type="button"
